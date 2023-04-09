9d0d063041c287dfdac49cdf43a9695f0b279d83
hhullen <lipeinspecial@gmail.com>
2023-04-10 01:32:57 +0700

    update: gpt-api exception handling

--- src/handlers/other_handlers.py
+++ src/handlers/other_handlers.py
@@ -9,18 +9,23 @@ router: Router = Router()
 async def gpt(message: Message):
 
     openai.api_key = os.environ['OPEN_AI_KEY']
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0301",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": message.text}
-        ]
-    )
-    answer = response['choices'][0]['message']['content']
-    if answer:
-        await message.answer(text=answer)
-    else:
-        message.answer(text="Smt went wrong, try to ask again")
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0301",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                # {"role": "system", "content": "You are my best friend."},
+                {"role": "user", "content": message.text}
+            ]
+        )
+        answer = response['choices'][0]['message']['content']
+        if answer:
+            await message.answer(text=answer)
+        else:
+            message.answer(text="Smt went wrong, try to ask again")
+    except:
+        message.answer(
+            text="I amfucking overloaded, probably. Try ask me the question later.")
 
 
 @router.message()
@@ -28,4 +33,4 @@ async def send_answer(message: Message):
     if message.text:
         await gpt(message)
     else:
-        await message.reply("It is not a test message, probably 🙊")
+        await message.reply("It is not a text message 🙊")
